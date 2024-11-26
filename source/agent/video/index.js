8ee558558ba57343724c6d14cee00a55b4e3d9ac
laplaceliu <laplaceliu@outlook.com>
2024-11-26 16:33:19 +0800

    WIP: new analyzer

--- source/agent/video/index.js
+++ source/agent/video/index.js
@@ -2,45 +2,35 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-"use strict";
+'use strict';
 
-const MediaFrameMulticaster = require("../mediaFrameMulticaster/build/Release/mediaFrameMulticaster");
+const MediaFrameMulticaster = require('../mediaFrameMulticaster/build/Release/mediaFrameMulticaster');
 
-const logger = require("../logger").logger;
+const logger = require('../logger').logger;
 
 // Logger
-const log = logger.getLogger("VideoNode");
+const log = logger.getLogger('VideoNode');
 
-const { InternalConnectionRouter } = require("./internalConnectionRouter");
+const { InternalConnectionRouter } = require('./internalConnectionRouter');
 const router = new InternalConnectionRouter(global.config.internal);
 
 // Setup GRPC server
-var createGrpcInterface = require("./grpcAdapter").createGrpcInterface;
+var createGrpcInterface = require('./grpcAdapter').createGrpcInterface;
 var enableGRPC = global.config.agent.enable_grpc || false;
 
-var EventEmitter = require("events").EventEmitter;
+var EventEmitter = require('events').EventEmitter;
 
 var useHardware = global.config.video.hardwareAccelerated,
   gaccPluginEnabled = global.config.video.enableBetterHEVCQuality || false,
   MFE_timeout = global.config.video.MFE_timeout || 0,
   supported_codecs = global.config.video.codecs;
 
-var VideoMixer, VideoTranscoder;
-try {
-  if (useHardware) {
-    VideoMixer = undefined;
-    VideoTranscoder = undefined;
-  } else {
-    VideoMixer = require("../videoMixer_sw/build/Release/videoMixer-sw");
-    VideoTranscoder = require("../videoTranscoder_sw/build/Release/videoTranscoder-sw");
-  }
-} catch (e) {
-  log.error(e);
-  process.exit(-2);
-}
-
-const { VMixer } = require("./vmixer");
-const { VTranscoder } = require("./vtranscoder");
+const VideoMixer = require('../videoMixer_sw/build/Release/videoMixer-sw');
+const VideoTranscoder = require('../videoTranscoder_sw/build/Release/videoTranscoder-sw');
+const VideoAnalyzer = require('../videoTranscoder_sw/build/Release/videoAnalyzer-sw');
+const { VMixer } = require('./vmixer');
+const { VTranscoder } = require('./vtranscoder');
+const { VAnalyzer } = require('./vanalyzer');
 
 module.exports = function (rpcClient, selfRpcId, parentRpcId, clusterWorkerIP) {
   const that = {
@@ -61,7 +51,7 @@ module.exports = function (rpcClient, selfRpcId, parentRpcId, clusterWorkerIP) {
     callback
   ) {
     if (processor === undefined) {
-      if (service === "mixing") {
+      if (service === 'mixing') {
         processor = new VMixer(
           rpcClient,
           clusterWorkerIP,
@@ -71,7 +61,7 @@ module.exports = function (rpcClient, selfRpcId, parentRpcId, clusterWorkerIP) {
         );
         processor.initialize(config, belongTo, controller, mixView, callback);
         that.__proto__ = processor;
-      } else if (service === "transcoding") {
+      } else if (service === 'transcoding') {
         processor = new VTranscoder(
           rpcClient,
           clusterWorkerIP,
@@ -81,17 +71,27 @@ module.exports = function (rpcClient, selfRpcId, parentRpcId, clusterWorkerIP) {
         );
         processor.initialize(config.motionFactor, controller, callback);
         that.__proto__ = processor;
+      } else if (service === 'analytic') {
+        processor = new VAnalyzer(
+          rpcClient,
+          clusterWorkerIP,
+          VideoAnalyzer,
+          router,
+          streamingEmitter
+        );
+        processor.initialize(config.motionFactor, controller, callback);
+        that.__proto__ = processor;
       } else {
-        log.error("Unknown service type to init a video node:", service);
+        log.error('Unknown service type to init a video node:', service);
         callback(
-          "callback",
-          "error",
-          "Unknown service type to init a video node."
+          'callback',
+          'error',
+          'Unknown service type to init a video node.'
         );
       }
     } else {
-      log.error("Repetitive initialization, service:", service);
-      callback("callback", "error", "Repetitive initialization.");
+      log.error('Repetitive initialization, service:', service);
+      callback('callback', 'error', 'Repetitive initialization.');
     }
   };
 
@@ -103,7 +103,7 @@ module.exports = function (rpcClient, selfRpcId, parentRpcId, clusterWorkerIP) {
   that.getInternalAddress = function (callback) {
     const ip = global.config.internal.ip_address;
     const port = router.internalPort;
-    callback("callback", { ip, port });
+    callback('callback', { ip, port });
   };
   if (enableGRPC) {
     // Export GRPC interface.
